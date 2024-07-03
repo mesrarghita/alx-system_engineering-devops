665415c871d8ff4fa346b8cb0c817e592ed4e3c0
mesrarghita <ghita1mesrar@gmail.com>
2024-07-03 16:00:20 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,13 +1,9 @@
-# Create a file named "school" in the "/tmp" directory
-file { '/tmp/school':
-  # Ensure that the file exists and is a regular file
-  ensure  => file,
-  # Set the file permission to 0744 (rwxr--r--)
-  mode    => '0744',
-  # Set the owner of the file to "www-data"
-  owner   => 'www-data',
-  # Set the group of the file to "www-data"
-  group   => 'www-data',
-  # Set the content of the file to "I love Puppet"
-  content => 'I love Puppet',
+# i love Puppet
+file {'/tmp/school':
+    ensure  => 'file',
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
