fc0f77200017a850bb59fd3ed89001fd79a165f4
mesrarghita <ghita1mesrar@gmail.com>
2024-08-21 12:01:23 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""that queries the Reddit API"""
+""" 1-top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed"""
-    headers = {
-        'User-agent': 'Holberton',
-    }
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    req = requests.get(url, headers=headers)
-
-    if (req.status_code != 200):
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print(None)
         return
-
-    try:
-        childrens = req.json().get('data').get('children')
-        for children in childrens:
-            print(children.get('data').get('title'))
-    except Exception:
-        pass
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
